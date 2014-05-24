c587978f8704ca83d8d3a1390d15fd7860b6b625
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-05-24 13:03:36 +0900

    first commit

---
+++ Arduino/bit/bit.ino
@@ -0,0 +1,18 @@
+
+const int ledPin =  9;      
+const int sensorPin = A0;    
+int time = 1000;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);  
+}
+
+void loop(){
+  time = map(analogRead(sensorPin),0,1023,300,5000); 
+  digitalWrite(ledPin, HIGH);
+  delayMicroseconds(time);
+  digitalWrite(ledPin, LOW);
+  delayMicroseconds(time);
+}
+
